428ea6a23c3a51d9e236f698b4b12d5b205a7469
Indroic <indroic@outlook.com>
2026-06-06 11:35:43 -0400

    ahora si va a funcionar

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -12,7 +12,7 @@ app.use(logger());
 app.use(
   "/*",
   cors({
-    origin: "*",
+    origin: "https://medical.indroic.dev",
     allowMethods: ["GET", "POST", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
