d32945292b6cd962eae099ab942e3b335401b8a0
Indroic <indroic@outlook.com>
2026-06-06 11:33:27 -0400

    actualizacion de los cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -12,7 +12,7 @@ app.use(logger());
 app.use(
   "/*",
   cors({
-    origin: env.CORS_ORIGIN,
+    origin: "*",
     allowMethods: ["GET", "POST", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
